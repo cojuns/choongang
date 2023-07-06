f16299d78d18b89a511edb3a32b1bc9c55b181e5
cojuns <122452171+cojuns@users.noreply.github.com>
2023-07-06 18:23:49 +0900

    Update DTO.java

--- 35day/개발지원서_DB에 값 넣기/DTO.java
+++ 35day/개발지원서_DB에 값 넣기/DTO.java
@@ -1,11 +1,13 @@
 package sec;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 
 @NoArgsConstructor
+@Getter
 @Setter
 @ToString
 public class BackDto {
