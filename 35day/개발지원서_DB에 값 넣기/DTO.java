f56dc0195aa7e8b6f3a6631a53ce883b77a9f070
cojuns <122452171+cojuns@users.noreply.github.com>
2023-07-06 18:23:04 +0900

    Create DTO.java

---
+++ 35day/개발지원서_DB에 값 넣기/DTO.java
@@ -0,0 +1,17 @@
+package sec;
+
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@NoArgsConstructor
+@Setter
+@ToString
+public class BackDto {
+	int num;
+	String name;
+	String phone;
+	String support;
+	String content;
+}
