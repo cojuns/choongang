b2e2cf28b5dd677a8f425cf03718bfc942dd6ee0
cojuns <122452171+cojuns@users.noreply.github.com>
2023-08-02 13:46:27 +0900

    Update Update.java

--- EX_Test_CRUD/Update.java
+++ EX_Test_CRUD/Update.java
@@ -11,3 +11,26 @@ public class UpdateOneEx {
 	}
 
 }
+
+
+
+	public void update(Member member) {
+	    String sql = "update member set password=?, name=?, email=? "
+	            + "where memberid = ?";
+
+	    jdbcTemplate.update(sql, // sql 쿼리문 순서대로
+	            member.getPassword(),
+	            member.getName(), member.getEmail(), member.getMemberid()
+	    );
+	    System.out.println("업데이트 완료");
+	}
+
+public class TestMemberDao {
+
+	public static void main(String[] args) { // DTO, DB 순서대로
+		
+			Member member = new Member("test4", "222", "신마적","ewewe@dd.com");
+		memberDao.update(member);
+		
+	}
+}
